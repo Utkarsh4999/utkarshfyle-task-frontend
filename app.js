3c60861512e1d49ab83a50acc0a445d2bbed58fd
Manthan <manthankoolwal2450@gmail.com>
2018-08-31 16:36:54 +0530

    fyle task completed

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+function getMovieData()  {
+
+
+  var input = document.querySelector(".pokemon-input");
+  var pokemonName = document.querySelector(".pokemon-name");
+  var pokemonGenre = document.querySelector(".pokemon-genre");
+  var movieCountry = document.querySelector(".movie-country");
+  var pokemonImage = document.querySelector(".pokemon-image");
+  var name = input.value;
+  movieUrl = `http://www.omdbapi.com/?t=${name}&apikey=2955d8c1`;
+
+  axios.get(movieUrl).then((response) => {
+    if(response.data.Response === 'False'){
+      throw new Error('Unable to find the movie');
+    }
+     pokemonName.innerHTML = "Name:"+response.data.Title;
+     pokemonGenre.innerHTML = "Genre:"+response.data.Genre;
+     movieCountry.innerHTML = "Country:"+response.data.Country;
+     pokemonImage.src =response.data.Poster;
+
+
+  }).catch((e) => {
+    pokemonName.innerHTML = "(An error has occurred.)";
+
+    pokemonImage.src = "";
+  });
+
+
+}
+
+var button = document.querySelector(".pokemon-button");
+button.addEventListener("click", getMovieData);
